d6aa5452c7d74e93d8b50202600835dcd2ddcc32
xdiegoemmax@outlook.com <xdiegoemmax@outlook.com>
2021-12-21 18:57:28 -0600

    Rails project base and database creation
    - Creation of the project in Rails
    - Creation of the database using the Rails Console
    - The foreign keys are yet to be configured
    
    Co-authored-by: armanirmr <armanirmr@gmail.com>
    Co-authored-by: YxMARCKxY <mramirez73@ucol.mx>
    Co-authored-by: rene3255 <ranchoeltecal@gmail.com>

---
+++ time-clock/db/schema.rb
@@ -0,0 +1,48 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_12_22_000227) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string "user"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "branches", force: :cascade do |t|
+    t.string "name"
+    t.string "direction"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "check_in_outs", force: :cascade do |t|
+    t.datetime "entrance"
+    t.datetime "out"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "position"
+    t.integer "branch_id"
+    t.integer "private_number"
+    t.string "email"
+    t.integer "check_in_out_id"
+    t.boolean "active"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
