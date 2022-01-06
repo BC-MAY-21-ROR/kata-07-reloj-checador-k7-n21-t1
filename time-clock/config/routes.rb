cefb8407a61263e4593048662330d26a15961b3c
diegoOG09 <xdiegoemmax@outlook.com>
2022-01-05 22:38:48 -0600

    Changes switched to feature#3 branch

--- time-clock/config/routes.rb
+++ time-clock/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get 'index', to: 'index#index'
+    get 'layouts/admin', to: 'layouts#admin'
+    get 'layouts/branches', to: 'layouts#branches'
+    get 'layouts/employees', to: 'layouts#employees'
 end
