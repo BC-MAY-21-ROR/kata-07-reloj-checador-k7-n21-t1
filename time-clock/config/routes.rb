1956b4dc6d5f11d7556afb865cdddc31bae54c98
Eduardo Hernandez <eduardoghdez.io@gmail.com>
2022-01-11 18:27:07 -0600

    Fix root/index route

--- time-clock/config/routes.rb
+++ time-clock/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-    get 'index', to: 'index#index'
-    get 'pages/admin', to: 'pages#admin'
-    get 'pages/branches', to: 'pages#branches'
-    get 'pages/employees', to: 'pages#employees'
+  root to: 'index#index'
+  get 'pages/admin', to: 'pages#admin'
+  get 'pages/branches', to: 'pages#branches'
+  get 'pages/employees', to: 'pages#employees'
 end
