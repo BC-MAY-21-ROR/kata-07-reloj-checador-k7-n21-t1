fa38dbe7dc520287a2510e810c024d023a915169
MARCELO RAMIREZ M <mramirez73@ucol.mx>
2022-01-14 19:33:35 -0600

    borrado de archivos que dan problema en el rebase

--- time-clock/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  root to: 'index#index'
-  get 'pages/admin', to: 'pages#admin'
-  get 'pages/branches', to: 'pages#branches'
-  get 'pages/employees', to: 'pages#employees'
-end
