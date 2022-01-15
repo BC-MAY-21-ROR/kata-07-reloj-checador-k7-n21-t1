ae33f7bfcc893cb8cbf4eecb4ab85e8db6b7bcdf
mantequilla <mantequilla@pop-os.localdomain>
2022-01-14 21:05:14 -0600

    hasta aqui todo bien todo correcto
    Please enter the commit message for your changes. Lines starting

--- time-clock/app/models/employee.rb
+++ time-clock/app/models/employee.rb
@@ -2,32 +2,3 @@ class Employee < ApplicationRecord
   belongs_to :branch
   has_many :checks
 end
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
- 
->>>>>>> e5c6800 (views and database)
-=======
-<<<<<<< Updated upstream
- 
-=======
-=======
->>>>>>> c4cabd3 (backup with devise)
-=======
->>>>>>> 132b374931ce7a2b848d49187dfa9c4bdefc3bda
-
-#rails g model user datos:string references:checks
-
-#checks
-
-#Employe.first.branch.name
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> Stashed changes
->>>>>>> 32d35a8 (the coppy more recient)
-=======
->>>>>>> c4cabd3 (backup with devise)
-=======
->>>>>>> 132b374931ce7a2b848d49187dfa9c4bdefc3bda
--- time-clock/db/-- SQLite.sql
+++ time-clock/db/-- SQLite.sql
@@ -1,59 +1,35 @@
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 -- INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Diego", "Developer", "email@gmail.com", true, 3, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 -- INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Rene", "Developer", "email@gmail.com", true, 2, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 -- INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Armani", "Developer", "email@gmail.com", true, 4, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 -- INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Marck", "Developer", "email@gmail.com", true, 1, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
-=======
+
 --  INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Diego", "Developer", "diego@gmail.com", true, 3, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 --  INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Rene", "Developer", "rene@gmail.com", true, 2, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 --  INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Armani", "Developer", "armani@gmail.com", true, 4, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 --  INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Marck", "Developer", "marck@gmail.com", true, 1, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 --  INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Irene", "Developer", "irene@gmail.com", true, 1, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
->>>>>>> 32d35a8 (the coppy more recient)
-=======
-=======
->>>>>>> 132b374931ce7a2b848d49187dfa9c4bdefc3bda
+
 INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Diego", "Developer", "diego@gmail.com", true, 3, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Rene", "Developer", "rene@gmail.com", true, 2, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Armani", "Developer", "armani@gmail.com", true, 4, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Marck", "Developer", "marck@gmail.com", true, 1, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO employees (name, position, email, active, branch_id, created_at, updated_at) VALUES ("Irene", "Developer", "irene@gmail.com", true, 1, "2022-01-05 08:30:00", "2022-01-05 08:30:00");
-<<<<<<< HEAD
->>>>>>> 132b374 (Cambios a devise before disaster)
-=======
->>>>>>> 132b374931ce7a2b848d49187dfa9c4bdefc3bda
 
 INSERT INTO branches (name, direction, created_at, updated_at) VALUES ("central","lomas verdes #34 emiliano zapata","2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO branches (name, direction, created_at, updated_at) VALUES ("anexo","lomas naranjas #43 bugambilias","2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO branches (name, direction, created_at, updated_at) VALUES ("foranea","lomas amarillas #66 escobera","2022-01-05 08:30:00", "2022-01-05 08:30:00");
 INSERT INTO branches (name, direction, created_at, updated_at) VALUES ("juarez","lomas azules #105 san antonio","2022-01-05 08:30:00", "2022-01-05 08:30:00");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 132b374 (Cambios a devise before disaster)
-=======
->>>>>>> 132b374931ce7a2b848d49187dfa9c4bdefc3bda
 INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 07:00:00","2022-01-05 08:00:00",11,"2022-01-05 08:30:00","2022-01-05 08:30:00");
 INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 08:00:00","2022-01-05 09:00:00",10,"2022-01-05 08:30:00","2022-01-05 08:30:00");
 INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 09:00:00","2022-01-05 10:00:00",12,"2022-01-05 08:30:00","2022-01-05 08:30:00");
 INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 10:00:00","2022-01-05 11:00:00",9,"2022-01-05 08:30:00","2022-01-05 08:30:00");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 -- INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 07:00:00","2022-01-05 08:00:00",11,"2022-01-05 08:30:00","2022-01-05 08:30:00");
 -- INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 08:00:00","2022-01-05 09:00:00",10,"2022-01-05 08:30:00","2022-01-05 08:30:00");
 -- INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 09:00:00","2022-01-05 10:00:00",12,"2022-01-05 08:30:00","2022-01-05 08:30:00");
 -- INSERT INTO checks (entrance, out, employee_id, created_at, updated_at) VALUES ("2022-01-05 10:00:00","2022-01-05 11:00:00",9,"2022-01-05 08:30:00","2022-01-05 08:30:00");
->>>>>>> 32d35a8 (the coppy more recient)
 
 
 -- DELETE from checks where id > 0;
-=======
->>>>>>> 132b374 (Cambios a devise before disaster)
-=======
->>>>>>> 132b374931ce7a2b848d49187dfa9c4bdefc3bda
